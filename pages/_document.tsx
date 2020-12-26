7c9fb1d62f4526d5fb419b9e9a96898ded662f22
Kayky de Brito <oss@kaykyb.com>
2020-12-26 00:52:47 -0300

    Add plausible

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,12 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/favicon.png" />
+          <script
+            async
+            defer
+            data-domain="kaykyb.com"
+            src="https://plausible.io/js/plausible.js"
+          ></script>
         </Head>
         <body>
           <Main />
